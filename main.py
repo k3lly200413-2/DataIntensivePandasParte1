f5d24ae8ffd856e9d318c87003fbe0a741d61c9f
Kelly Applebee <kelly.applebee@studio.unibo.it>
2026-03-22 15:46:24 +0100

    feat: print to see info about data

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def main():
     DATA_URL = "https://github.com/datascienceunibo/dialab2024/raw/main/Preprocessing_con_pandas/usa_census.npz"
 
@@ -5,6 +8,10 @@ def main():
     if not os.path.exists("usa_census.npz"):
         from urllib.request import urlretrieve
         urlretrieve(DATA_URL, "usa_census.npz")
+    
+    data = np.load("usa_census.npz")
+    for name, array in data.items():
+        print(name, array.dtype, array.shape)
 
 
 if __name__ == "__main__":
