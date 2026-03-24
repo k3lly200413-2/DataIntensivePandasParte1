e97617859b50648d5558074425ac9807afa6fd22
Kelly Applebee <kelly.applebee@studio.unibo.it>
2026-03-24 14:01:38 +0100

    feat: show attributes of pd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import pandas as pd
 
 def main():
     DATA_URL = "https://github.com/datascienceunibo/dialab2024/raw/main/Preprocessing_con_pandas/usa_census.npz"
@@ -10,11 +10,19 @@ def main():
         urlretrieve(DATA_URL, "usa_census.npz")
     
     data = np.load("usa_census.npz")
-    for name, array in data.items():
-        print(f"{name:>15}: {array.dtype!s:>8} {array.shape}")
+    # for name, array in data.items():
+        # print(f"{name:>15}: {array.dtype!s:>8} {array.shape}")
         # "!s" = converti in stringa con str(x) in modo da poter applicare formato
         # ">N" = riserva N caratteri e allinea a destra
 
+    population = pd.Series(data["population"], index=data["states"])
+    
+    print(population.head(7))   
+    
+    print(population.values[:5])
+    print(population.index[:5])
+    print(len(population))
+    print(population.size)
 
 if __name__ == "__main__":
     main()
